0ad49d6036ae0365addc576bb153cf42ffbdf347
kavv210 <kaavyagera2101@gmail.com>
2025-06-18 02:48:35 +0530

    Update shop.js

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -10,13 +10,9 @@ import Icon from '../components/Icons/Icon';
 import Layout from '../components/Layout';
 import LayoutOption from '../components/LayoutOption';
 import ProductCardGrid from '../components/ProductCardGrid';
-import { generateMockProductData } from '../helpers/mock';
 import Button from '../components/Button';
 import Config from '../config.json';
 
-const ShopPage = (props) => {
-  const [showFilter, setShowFilter] = useState(false);
-  const data = generateMockProductData(6, 'woman');
 
   useEffect(() => {
     window.addEventListener('keydown', escapeHandler);
